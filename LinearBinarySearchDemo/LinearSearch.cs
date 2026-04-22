0840fea6cb3515949ed095b9f4d82974662a3e1a
Amit Pandey <amitpandeymtr2002@gmail.com>
2026-04-22 11:15:46 +0530

    Add project files.

---
+++ LinearBinarySearchDemo/LinearSearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class LinearSearchExample
+{
+    static int LinearSearch(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter the elements:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Element {i}: ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Enter number to search: ");
+        int target = Convert.ToInt32(Console.ReadLine());
+
+        int result = LinearSearch(arr, target);
+
+        if (result != -1)
+        {
+            Console.WriteLine("Element found at index: " + result);
+        }
+        else
+        {
+            Console.WriteLine("Element not found.");
+        }
+    }
+}
