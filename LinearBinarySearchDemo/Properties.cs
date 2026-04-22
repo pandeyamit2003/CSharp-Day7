e2b02dbf2442632651b6e3b9239040f42ce147c8
Amit Pandey <amitpandeymtr2002@gmail.com>
2026-04-22 14:12:04 +0530

    Add files via upload

---
+++ LinearBinarySearchDemo/Properties.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Person
+{
+    private string name;
+    private int age;
+
+    // Property for Name
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+
+    // Property for Age (with validation)
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age cannot be negative.");
+            }
+            age = value;
+        }
+    }
+}
+
+class Properties
+{
+    static void Main()
+    {
+        Person person = new Person();
+
+        // Setting values using properties
+        person.Name = "Amit";
+        person.Age = 23;
+
+        // Getting values using properties
+        Console.WriteLine("Name: " + person.Name);
+        Console.WriteLine("Age: " + person.Age);
+    }
+}
